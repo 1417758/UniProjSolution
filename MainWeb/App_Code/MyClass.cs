acdb425bf2fc8b6768c7390521583c20eb50b253
1417758 <1417758@rgu.ac.uk>
2016-06-29 12:16:56 +0100

    added class to test asp.net Identity2

---
+++ MainWeb/App_Code/MyClass.cs
@@ -0,0 +1,35 @@
+﻿using MainWeb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for MyClass
+/// </summary>
+public class MyClass : ApplicationUser {
+
+    //variables
+    public string First { get; set; }
+    public string Last { get; set; }
+    public string Age { get; set; }
+    public string EmailReg { get; set; }
+
+
+    //constructor
+    public MyClass()  {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    //public MyClass(ApplicationUser webUser);
+
+    protected void CreateUser_Click(object sender, EventArgs e) {
+        var manager = new UserManager();
+        var endUser = new ApplicationUser() { UserName = "Rachie" };
+    }
+
+
+
+
+}
