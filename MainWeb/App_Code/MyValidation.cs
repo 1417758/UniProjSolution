3f004415688105ca659e07c17b3fcb2436c55926
1417758 <1417758@rgu.ac.uk>
2016-06-29 12:17:26 +0100

    added class to test asp.net Identity2

---
+++ MainWeb/App_Code/MyValidation.cs
@@ -0,0 +1,63 @@
+﻿using MainWeb;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Summary description for MyValidation
+/// </summary>
+public class MyValidation {
+    public MyValidation() {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+
+
+
+    public class MyPasswordValidator : IIdentityValidator<string> {
+        private PasswordValidator passValid;
+        private UserManager userManage;
+
+        //constructor 1
+        public MyPasswordValidator(UserManager userManager) {
+            this.userManage = userManager;
+        }
+
+        //constructor 1
+        public MyPasswordValidator(PasswordValidator passwordValidator) {
+            this.passValid = passwordValidator;
+        }
+        //constructor 2
+        public MyPasswordValidator() {
+            this.passValid.RequireLowercase = true ;
+            this.passValid.RequireUppercase = false;
+            this.passValid.RequireDigit = true;
+            this.passValid.RequireNonLetterOrDigit = false;
+            this.passValid.RequiredLength = 8;
+        }
+
+        public Task<IdentityResult> ValidateAsync(string item) {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MyUserValidator : IIdentityValidator<ApplicationUser> {
+        private UserManager userManager;
+
+        public MyUserValidator(UserManager userManager) {
+            this.userManager = userManager;
+        }
+
+        public Task<IdentityResult> ValidateAsync(ApplicationUser item) {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+}
