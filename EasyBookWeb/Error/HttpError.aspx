380ddff309b7154c49568222170cac4a3e87fb56
1417758 <1417758@rgu.ac.uk>
2016-07-02 12:49:02 +0100

    added http error page

---
+++ EasyBookWeb/Error/HttpError.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Error/ErrorMaster.master" AutoEventWireup="true" CodeFile="HttpError.aspx.cs" Inherits="Error_HttpError" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" Runat="Server">
+
+     <div class="jumbotron">
+        <h1>HTTP Error Page</h1>
+        <p class="lead">
+            bla bla bla
+        </p>
+        <p>
+            Return to <a href='TestError.aspx' class="btn btn-primary btn-lg">Test Error Page  &raquo;</a>
+        </p>
+    </div>
+
+
+    <section class="row">
+        <div id="div1" class="col-md-6">
+            <h2>Inner Error Message</h2>
+            <p>
+                <asp:Label ID="lblInnerMessage" runat="server" Font-Bold="true"
+                    Font-Size="Large" />
+            </p>
+            <%--  <br />--%>
+            <pre>
+                <asp:Label ID="lblInnerTrace" runat="server" />
+            </pre>
+
+        </div>
+        <div id="div2" class="col-md-6">
+            <h2>Error Message</h2>
+            <p>
+                <asp:Label ID="lblExMessage" runat="server" Font-Bold="true"
+                    Font-Size="Large" />
+            </p>
+            <pre>
+              <asp:Label ID="lblExTrace" runat="server" Visible="true" />
+            </pre>
+        </div>
+
+    </section>
+
+</asp:Content>
+
---
+++ EasyBookWeb/Error/HttpError.aspx.cs
@@ -0,0 +1,57 @@
+﻿using EasyBookWeb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Error_HttpError : System.Web.UI.Page {
+
+    protected HttpException ex = null;
+    protected void Page_Load(object sender, EventArgs e) {
+
+        try {
+            ex = (HttpException)Server.GetLastError();
+            int httpCode = ex.GetHttpCode();
+
+            // Filter for Error Codes and set text
+            if (httpCode >= 400 && httpCode < 500)
+                ex = new HttpException
+                    (httpCode, "Safe message for 4xx HTTP codes.", ex);
+            else if (httpCode > 499)
+                ex = new HttpException
+                    (ex.ErrorCode, "Safe message for 5xx HTTP codes.", ex);
+            else
+                ex = new HttpException
+                    (httpCode, "Safe message for unexpected HTTP codes.", ex);
+
+            // Log the exception and notify system operators
+            ExceptionUtility.LogException(ex, "HttpError Page, Page_Load Event");
+            ExceptionUtility.NotifySystemOps(ex);
+
+            // Fill the page fields
+            lblExMessage.Text = ex.Message;
+            lblExTrace.Text = ex.StackTrace;
+
+            // Show Inner Exception fields for local access
+            if (ex.InnerException != null) {
+                lblInnerTrace.Text = ex.InnerException.StackTrace;
+                //InnerErrorPanel.Visible = Request.IsLocal;
+                lblInnerMessage.Text = string.Format("HTTP {0}: {1}",
+                  httpCode, ex.InnerException.Message);
+            }
+            // Show Trace for local access
+            lblExTrace.Visible = Request.IsLocal;
+
+            // Clear the error from the server
+            Server.ClearError();
+        }
+        catch (Exception ex) {
+            System.Diagnostics.Debug.Print("<h2>HttpError.aspx, Page_Load Event</h2>\n" + ex.ToString() + "\n" + ex.InnerException + "\n" + ex.Message);
+            // Log the exception and notify system operators
+            ExceptionUtility.LogException(ex, "HttpError.aspx, Page_Load Event");
+            ExceptionUtility.NotifySystemOps(ex);
+        }
+    }
+}
