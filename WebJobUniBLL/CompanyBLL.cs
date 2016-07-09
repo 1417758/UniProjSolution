e396e640d7335ffb59db1f5b40ab9984e2a6e9cb
1417758 <1417758@rgu.ac.uk>
2016-07-09 09:46:51 +0100

    added new classes inhering from DAL

---
+++ WebJobUniBLL/CompanyBLL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebJobUniDAL;
+
+namespace WebJobUniBLL {
+    class CompanyBLL : Company {
+
+        //default constructor
+        public CompanyBLL() {
+        }
+
+    }//class
+}//namespace
---
+++ WebJobUniBLL/EndUserBLL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebJobUniDAL;
+
+namespace WebJobUniBLL {
+    class EndUserBLL : EndUser{
+
+        //default constructor
+        public EndUserBLL() {
+        }
+
+    }//class
+}//namespace
